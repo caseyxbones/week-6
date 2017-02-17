cf1b01daabccfcbb35223de56a6a0b870699f08e
Nathan Zimmerman <npzimmerman@gmail.com>
2017-02-17 10:28:44 -0500

    Clarifications for lab2

--- lab/lab2/js/main.js
+++ lab/lab2/js/main.js
@@ -1,4 +1,21 @@
 /* =====================
+Leaflet Configuration
+===================== */
+
+var map = L.map('map', {
+  center: [40.000, -75.1090],
+  zoom: 11
+});
+var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: 'abcd',
+  minZoom: 0,
+  maxZoom: 20,
+  ext: 'png'
+}).addTo(map);
+
+
+/* =====================
 
 ## Task 1
 
@@ -105,6 +122,20 @@ var myStyle = function(feature) {
   return {};
 };
 
+var showResults = function() {
+  /* =====================
+  This function uses some jQuery methods that may be new. $(element).hide()
+  will add the CSS "display: none" to the element, effectively removing it
+  from the page. $(element).show() removes "display: none" from an element,
+  returning it to the page. You don't need to change this part.
+  ===================== */
+  // => <div id="intro" css="display: none">
+  $('#intro').hide();
+  // => <div id="results">
+  $('#results').show();
+};
+
+
 var eachFeatureFunction = function(feature, layer) {
   layer.on('click', function (e) {
     /* =====================
@@ -131,30 +162,3 @@ $(document).ready(function() {
     }).addTo(map);
   });
 });
-
-var showResults = function() {
-  /* =====================
-  This function uses some jQuery methods that may be new. $(element).hide()
-  will add the CSS "display: none" to the element, effectively removing it
-  from the page. $(element).hide() removes "display: none" from an element,
-  returning it to the page. You don't need to change this part.
-  ===================== */
-  $('#intro').hide();
-  $('#results').show();
-};
-
-/* =====================
-Leaflet Configuration
-===================== */
-
-var map = L.map('map', {
-  center: [40.000, -75.1090],
-  zoom: 11
-});
-var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  subdomains: 'abcd',
-  minZoom: 0,
-  maxZoom: 20,
-  ext: 'png'
-}).addTo(map);
