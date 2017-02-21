92e45c5700e84b1be17d673b90947e23d4495cd8
Casey C. Ross <caseycross718@gmail.com>
2017-02-21 17:43:17 -0500

    Midterm Work In Progress
    midtermFunctions.js in progress, trying to get Indigo bike share
    station data into a format where I can see it and start planning for
    how to use it.
    
    basicApplication.js is done.
    
    basicApplication.html has been updated to read jQuery, leaflet, and
    underscore libraries.

--- assignment/basicApplication.js
+++ assignment/basicApplication.js
@@ -15,38 +15,61 @@ console. For example, try running: clickNextButton() and see what it does. Use l
 ================================ */
 
 var state = {
-  "slideNumber": 0, // slideNumber keeps track of what slide you are on. It should increase when you
+  "slideNumber": 1, // slideNumber keeps track of what slide you are on. It should increase when you
                     // click the next button and decrease when you click the previous button. It
                     // should never get so large that it is bigger than the dataset. It should never
                     // get so small that it is smaller than 0.
   "slideData": [
     {
-      "name": "Leaflet",
+      "name": "Slide 1",
       "language": "Javascript",
-      "namespace": "L"
+      "description": "description of slide 0"
     },
     {
-      "name": "Underscore",
+      "name": "Slide 2",
       "language": "Javascript",
-      "namespace": "_"
+      "description": "description of slide 1"
     },
     {
-      "name": "jQuery",
+      "name": "Slide 3",
       "language": "Javascript",
-      "namespace": "$"
+      "description": "description of slide 2"
+    },
+    {
+      "name": "Slide 4",
+      "language": "Javascript",
+      "description": "description of slide 3"
+    },
+    {
+      "name": "Slide 5",
+      "language": "Javascript",
+      "description": "description of slide 4"
     }
   ]
 };
+// console.log (state);
 
-var clickNextButton = function() {
-
-}
+var clickNextButton = function(event) {
+  if (state.slideNumber < state.slideData.length) {
+    state.slideNumber = (state.slideNumber + 1);
+    console.log("Slide" + " " + state.slideNumber);
+  }
+  else {
+    console.log("You are on the last slide!");
+  }
+};
 
 var clickPreviousButton = function() {
+  if (state.slideNumber > 1) {
+    state.slideNumber = (state.slideNumber - 1);
+    console.log("Slide" + " " + state.slideNumber);
+  }
+  else {
+    console.log("You are on the first slide!");
+  }
+};
 
-}
-
-var saySlideName = function(slide) {
-  // saySlideName uses console.log to "say" the name of the slide it is given. It should run when
-  // someone clicks on one of the buttons.
-}
+var saySlideName = function() {
+  var slideDataInfo = state.slideNumber;
+  console.log("You are on slide" + " " + [slideDataInfo]);
+};
--- assignment/midtermFunctions.js
+++ assignment/midtermFunctions.js
@@ -1,3 +1,13 @@
 /* ================================
 Week 6 Assignment: Midterm Functions + Signatures
 ================================ */
+
+
+
+var indegoStationUrl = "https://www.rideindego.com/stations/json/";
+
+var downloadData = $.ajax(indegoStationUrl);
+
+var parseData = function(ajaxResponseValue) {
+  return(JSON.parse(ajaxResponseValue));
+};
